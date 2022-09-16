212f9ae437e4695b2d08e1bcc70df9d5c6f1d6c5
Chris Couto <couchris@sheridancollege.ca>
2022-09-15 21:00:33 -0400

    first commit

--- src/components/PaymentCard.jsx
+++ src/components/PaymentCard.jsx
@@ -1,57 +1,60 @@
 import React, { Component, useEffect } from "react";
-import * as setup from "../config/hash.php";
+import axios from 'axios'
+import * as crypto from 'crypto';
 var GlobalPayments = window.GlobalPayments;
+
+
 
 const PaymentCard = () => {
   console.log("configured", window.GlobalPayments);
 
   useEffect(async () => {
-    window.GlobalPayments.configure({
-      merchantId: "MER_7e3e2c7df34f42819b3edee31022ee3f",
-      account: "TRA_c9967ad7d8ec4b46b6dd44a61cde9a91",
-      hash: function (request) {
-        return setup;
+
+    let appId = 'nlT1RcALpWibf2G7bl2KcMQoKxjdLiXz'
+    let appKey = 'wxbAtPNQmy4Y2Njb'
+    let nonce = new Date()
+    let secret = crypto.createHash('sha512').update(nonce+appKey).digest('hex');
+
+    let response = await axios({
+      method: 'post',
+      url: 'https://apis-qa.globalpay.com' + '/ucp/accesstoken',
+      headers: {
+        "X-GP-Version": '2020-10-22'
       },
-      env: "sandbox",
+      data: {
+        app_id: appId,
+        secret: secret,
+        grant_type: 'client_credentials',
+        nonce: nonce,
+        interval_to_expire: "1_HOUR"
+      
+      }
     });
 
-    const cardForm = GlobalPayments.creditCard.form("#credit-card");
+    console.log(response)
 
-// form-level event handlers. examples:
-cardForm.ready(() => {
-  console.log("Registration of all credit card fields occurred");
-});
-cardForm.on("token-success", (resp) => {
-  // add payment token to form as a hidden input
-  const token = document.createElement("input");
-  token.type = "hidden";
-  token.name = "payment-reference";
-  token.value = resp.paymentReference;
 
-  // submit data to the integration's backend for processing
-  const form = document.getElementById("payment-form");
-  form.appendChild(token);
-  form.submit();
-});
-cardForm.on("token-error", (resp) => {
-  // show error to the consumer
-});
+    window.GlobalPayments.configure({
+      accessToken: response.data,
+      env: "sandbox"
+    });
 
-// field-level event handlers. example:
-cardForm.on("card-number", "register", () => {
-  console.log("Registration of Card Number occurred");
-});
+    GlobalPayments.on("error", function (error) {
+      console.error(error);
+    });
+
+    var cardForm = GlobalPayments.creditCard.form('#credit-card-form', { style: "gp-default" });
+
+    cardForm.on("token-success", function (resp) { console.log(resp); });
+    cardForm.on("token-error", function (resp) { console.log(resp); });
   
   }, []);
 
   return (
     <>
-      <form id="payment-form" action="/charge" method="get">
-        <label for="billing-zip">Billing Postal Code</label>
-        <input id="billing-zip" name="billing-zip" type="tel" />
 
-        <div id="credit-card"></div>
-      </form>
+        <div id="credit-card-form"></div>
+      
 
     </>
   );
